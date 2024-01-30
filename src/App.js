8c98b88b998f4849e4cabbf87276c4fc1ef792a5
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-30 20:17:41 +0100

    imports for app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ import {
 
 import Footer from "./componets/home/Footer/Footer";
 import FooterBottom from "./componets/home/Footer/FooterBottom";
+import Header from "./componets/home/Header/Header";
+import HeaderBottom from "./componets/home/Header/HeaderBottom";
+import SpecialCase from "./componets/SpecialCase/SpecialCase";
+import About from "./pages/About/About";
+import SignIn from "./pages/Account/SignIn";
+import SignUp from "./pages/Account/SignUp";
+import Cart from "./pages/Cart/Cart";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Journal from "./pages/Journal/Journal";
+import Offer from "./pages/Offer/Offer";
+import Payment from "./pages/payment/Payment";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
   return (
