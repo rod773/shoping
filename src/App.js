48fe3f67b8317d4619d6c55174635cb0c349d171
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-30 19:50:26 +0100

    updates App,js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  createRoutesFromElements,
+  Routes,
+  ScrollRestoration,
+} from "react-router-dom";
+
+import Footer from "./componets/home/Footer/Footer";
+import FooterBottom from "./componets/home/Footer/FooterBottom";
 
 function App() {
   return (
